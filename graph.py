13a5dea760eda03910441cb8464ad8e11735c1ee
Velu Sankaran <kulanthaivelu.sankaran@walmartlabs.com>
2025-06-18 17:15:13 -0400

    personal assistant

---
+++ graph.py
@@ -0,0 +1,65 @@
+import os
+from typing_extensions import TypedDict, Annotated
+from langgraph.graph.message import add_messages
+from langgraph.graph import StateGraph, START, END
+from langchain_core.messages import AnyMessage, RemoveMessage
+from langchain_openai import ChatOpenAI
+from langchain_core.runnables import RunnablePassthrough
+from langchain_core.prompts import PromptTemplate
+from langgraph.checkpoint.memory import InMemorySaver
+from prompt import prompt
+
+class State(TypedDict):
+    """
+    Represents the state of the agent.
+    """
+    messages: Annotated[list[AnyMessage], add_messages]
+    
+def clean_messages(state: State) -> State:
+    """
+    Cleans the messages in the state except for the last one.
+    Args:
+        state (State): The current state of the agent.
+    Returns:
+        state (State): The updated state with only the last message retained.
+    """
+    messages = state["messages"]
+    last_message = messages[-1]
+    rest_messages = messages[:-1] if len(messages) > 1 else []
+    
+    return {"messages": [RemoveMessage(id=m.id) for m in rest_messages] + [last_message]}
+
+def chatbot(state: State) -> str:
+    """
+    Contacts LLM to get a response based on the current state.
+    Args:
+        state (State): The current state of the agent.
+    Returns:
+        state (State): The updated state with the response.
+    """
+    params = {
+        "azure_endpoint": os.getenv("AZURE_ENDPOINT"),
+        "azure_deployment": os.getenv("AZURE_DEPLOYLMENT"),
+        "api_version": os.getenv("MODEL_API_VERSION"),
+        "api_key": os.getenv("AZURE_API_KEY"),
+        "timeout": 60,
+    }
+    model = ChatOpenAI(
+        model="gpt-4.1-mini"
+    )
+
+    chain = {"query": RunnablePassthrough()} | prompt | model
+    
+    messages = state["messages"]
+    response = chain.invoke(messages)
+    return { "messages": [response] }
+
+builder = StateGraph(State)
+
+builder.add_node('chatbot', chatbot)
+builder.add_edge(START, 'chatbot')
+builder.add_edge('chatbot', END)
+
+checkpointer = InMemorySaver()
+
+graph = builder.compile()
---
+++ prompt.py
@@ -0,0 +1,126 @@
+from langchain_core.prompts import PromptTemplate
+
+prompt = PromptTemplate.from_template("""You are a helpful assistant that assists with requests from people who asks questions about me and my resume.
+I love to do coding, and I am a software architect.
+I have a lot of experience in software development, and I am always eager to learn new technologies.
+I have worked on various projects, including web applications, mobile applications, and cloud-based solutions.
+My resume is given below in markdown format:
+# Name
+Kulanthaivelu Sankaran
+
+## Title
+Full Stack Architect
+
+## Summary
+- 18+ Years
+- Full Stack Architect
+- Develop, Design, Performance Optimization, Cache Management, CDN Tuning, SEO Metrics, Page Render Choreography, Solution Architecture, Load Analysis
+- JavaScript, ES6, ReactJS, NodeJS, ExpressJS, HapiJS, Sinon, Lab, Code, Karma, Jasmine, Grunt, Gulp, SASS/SCSS, TypeScript, jQuery, AngularJS, Angular2, Polymer, Backbone, Java, J2EE, SpringMVC, Struts, PHP, Drupal, Apache, Tomcat, JBoss, tcServer, Akamai, Oracle, MongoDB, MySQL, SQLServer
+- API integrations: Facebook, Twitter, Google, Weather, Slack
+- Domains: Retail, Weather, Hotel
+- AI: Agentic AI, LangGraph, MCP Tools, A2A
+
+## Experience
+### ADP
+Principal Application Architect
+Jan 2018 - Present · 7 yrs 6 mos
+Alpharetta, Georgia
+- Design, Architect and Develop applications that will enable ADP third party app developers to take advantage of ADP data through secure API infrastructure, ADP Marketplace.
+- Developed multiple homegrown apps using the ADP marketplace infrastructure (Facebook Workplace, Microsoft Teams, Slack, Outlook)
+- Developed integration manager, a series of micro services to integrate any two ADP partners using any protocol followed in each of them.
+- Developed Developer Self Service that helps third party app developers to boot camp ADP apps with ease. The app allows the app developer to create applications, generate certificates, create secure credentials, create dev sandboxes and a whole lot of other goodies.
+- Developed homegrown content management system which creates/manages content in ADP developers portal.
+- Developed nextgen version of integration manager, which expanded on the original idea to integrate two partners but allows the partner to integrate on a deeper level.
+- Currently working on Agentic AI, creating MCP Tools in Agents using A2A Protocol. 
+
+### The Weather Channel
+Senior FullStack Web Developer
+Apr 2017 - Dec 2017 · 9 mos
+Atlanta, Georgia
+- Help the team decouple a NodeJS app from Drupal.
+- Migrate from AngularJS 1.x to React based isomorphic application.
+- Using Saga and Redux, create a store of data from backend and hydrate the DOM on the client side. 
+- Create a modular npm repository to have each module deployable when its ready without having a huge release.
+- Rewrite APIs in Go Lang to increase the performance of backend calls.
+- Also, work on creating microservice architecture where each module can be rendered separately and scaled through AWS.
+- Technology Stack: React (Inferno), KoaJS, Redux, Saga, Webpack, Rollup
+- Concurrently, involved in redesigning weather widgets - possibly with Angular 4 or Inferno or Go lang.
+
+### IHG Hotels & Resorts
+Web Architect / Lead
+Jun 2015 - Mar 2017 · 1 yr 10 mos
+Atlanta Metropolitan Area
+- Team Lead/Architect for the multibrand/AccountManagement/EnhanceYourStay section of the website.
+- Architect, design and develop projects that are approved by business and product.
+- Worked on migrating backend solutions to REST based microsite architecture stack and was successful.
+- Test Driven Development to ensure modules are unit tested and satisfy code coverage criteria.
+- Trained the team on front-end stack with AngularJS, Karma, Grunt and NodeJS.
+- Conducted research on performance optimization with topics such as migration to NPM3, migration to GemFire, website tuning and have successfully implemented them.
+
+### The Weather Channel
+Web Developer
+May 2012 - May 2015 · 3 yrs 1 mo
+Atlanta Metropolitan Area
+- Core team member of Digital Infrastructure Reboot project - a complete rewrite of weather.com from backend stack to frontend stack.
+- Worked on developing a SEO solution for JavaScript sites using phantomjs.
+- Developed the core data access layer (DAL) to make calls to the data services using a single factory interface.
+- Developed RESTangular like model interface for weather.com so any change in events like unit preferences will refresh the UI seamlessly.
+- Developed core entities in Drupal which can used to maintain CMS asset queries..
+- Developed core content surfacing module which provides various templates across one content querying engine.
+- Design and develop components/modules in the website (www.weather.com).
+- Redesigned the ad controller on the website to use Google DFPP.
+- Involved in "My Friends' Weather" project, a social association of the weather channel with Facebook which shows the Facebook friends who are affected by bad weather. 
+
+Technologies Worked Here: Javascript, Java, JSP, JSTL, JSON, AJAX, jQuery, AngularJS, ReactJS, Backbone, DUST, PHP, Drupal, SASS, Grunt, PhantomJS, NodeJS.
+
+### Akamai Technologies
+Senior Application Software Engineer
+Feb 2011 - Apr 2012 · 1 yr 3 mos
+- Designed, developed, tested and deployed core platform tools at Akamai.
+- Led a team of six while coding the critical components and also designed web support along with execution-engine for execution of workflows from a web UI (J2EE).
+- Responsible for REST service platform development of the application.
+- Worked towards achieving the goal “Code Once, Run Anywhere” and was successful.
+- Attended business meetings to gather requirements, clarify usage queries and worked with offshore teams to provide proper design plan to the team.
+- Eradicated many problems like synchronization, unusual UI behavior for certain objects. 
+- Worked in a highly cross-functional capacity, partnering with product management, QA, technical writers, and service delivery professionals.
+- Created new features from conception through release and enhance existing applications to ensure that Akamai customers have an exceptional experience using the Edge Control Portal.
+- Recruited, mentored, and led a team of talented engineers to their full potential.
+- Inspired engineers to work hard, led by example and keep enthusiasm high in tight-deadline situations.
+- Oversaw technical scoping and design of projects and ensure projects meet expectations on time.
+- Worked closely with management to identify risks, solve problems, and maintain progress.
+
+### Amazon
+Application Engineer
+Jul 2009 - Feb 2011 · 1 yr 8 mos
+- The project demands sound technical knowledge on: Java, Perl, CGI and Oracle 9i.
+- Responsible for development and maintenance of Services which are used by the breakthrough device by Amazon - Kindle.
+- Worked with the device team which handles requests like device registration, device first radio contact, content delivery and annotations management.
+- Responsible for content delivery in less than 60 seconds – challenging task when the number of content and size of content being delivered.
+- Responsible for creation of tools and other various helping materials to help out operations team.
+- Involved in project beta meetings to drive the projects to completion while accommodating the comments from beta users during the course.
+- Have worked closely with Technical Architects for designing the cutting edge services.
+
+### Best Buy
+Application Engineer
+Jul 2006 - Jul 2009 · 3 yrs 1 mo
+Greater Minneapolis-St. Paul AreaGreater Minneapolis-St. Paul Area
+- Delivered critical projects to bestbuy.com, including RWZ Membership project, Opera performance tune project and Critical jobs “Delta” run.
+- Delivered the critical projects before the holiday seasons and gather requirements with client for the upcoming projects.
+- Possess sound knowledge of technical subjects and have conducted knowledge sharing sessions for the team.
+- Played a role of grooming a team technically from the scratch and made them scale up to expectations.
+- Have led a six member team and successfully delivered projects.
+- Allocated work to team members and tracked it to completion.
+- Have been involved in warranty support (after every project release) and holiday support (during holiday season). This helped to understand the functional architecture and thereby helped me to reach the next level - Leadership.
+- Involved in development of new functionalities/enhancement of existing functionalities in Best Buy. 
+- Involved in analysis, design, coding and test demanding knowledge on: ATG Commerce, JAVA, HTML, JSP, CSS and DOM.
+- Involved in UI design for the projects: ABN, CED and Button Messaging.
+- ABN: Attribute based navigation – an initiative for customer experience helping them out to choose the product they wanted sooner and faster.
+- CED: Changes to the UI in product listing and product description pages. Again, an initiative to improve customer experience.
+- Button Messaging: Theoretically, a functional change but 90% of the project are changes to UI. It’s an enhanced way by which business can control buttons in bestbuy.com.
+- Critical projects involved: RWZ Certificates, ROS Bundling, Free Shipping and ABN (Attribute Based Navigation).
+- RWZ Certificates: Introduction of new discount type in bestbuy.com.
+- ROS Bundling: Introduction of new product packages in bestbuy.com.
+- Worked extensively in ATG on the following areas: Development of numerous Droplets, Form Handlers, and DSP and XmlRpcService components.
+                                      
+User Query: {query}
+""")
